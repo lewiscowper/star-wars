085d07289c79c3db8ff4c029dd5239fec6cff7bf
Lewis Cowper <lewis.cowper@googlemail.com>
2015-11-14 17:28:43 +0100

    fix: replace for loop

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ module.exports = function (array) {
     return false;
   }
 
-  for (var i = array.length - 1; i >= 0; i--) {
-    if (array[i] === '--force' || array[i] === 'force') {
-      return true;
-    }
+  if (array.indexOf('force') > -1 || array.indexOf('--force') > -1) {
+    return true;
+  } else {
+    return false;
   }
-
-  return false;
 };
